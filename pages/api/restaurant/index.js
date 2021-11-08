6dee2e46b7c0ddaeb89ffb067b965a98c6031563
KevProDev <kevintheleader@yahoo.com>
2021-11-07 22:49:43 -0600

    deleting some old files

--- pages/api/restaurant/index.js
+++
@@ -1,8 +0,0 @@
-// import nc from "next-connect";
-// import restaurants from "../../../src/data/restaurants";
-
-// const handler = nc().get((req, res) => {
-//   res.json({ data: restaurants });
-// });
-
-// export default handler;
