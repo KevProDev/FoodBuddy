be693c90a7833443bba79db2e726865ceafd8045
KevProDev <kevintheleader@yahoo.com>
2021-11-19 17:52:16 -0600

    lol i fix the nav fully

--- components/Header.js
+++ components/Header.js
@@ -14,7 +14,7 @@ import { Transition } from "@headlessui/react";
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 shadow-md py-5 px-5 md:px-10 bg-white md:grid-cols-6">
+    <header className="sticky top-0 z-50 grid grid-cols-2 shadow-md py-5 px-5 md:px-10 bg-white md:grid-cols-6">
       {/* Left */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto col-span-1">
         <Image
@@ -32,7 +32,7 @@ export default function Header() {
         <li>Saves</li>
       </div>
       {/* Right */}
-      <div className="flex items-center space-x-4 justify-end text-gray-600 col-span-2">
+      <div className="flex items-center space-x-4 justify-end text-gray-600 col-span-1 sm:col-span-1">
         <p className="hidden lg:inline-flex cursor-pointer text-lg">Sign In</p>
         {/* <GlobeAltIcon className="h-6 cursor-pointer" /> */}
         <button
@@ -84,9 +84,9 @@ export default function Header() {
                 Home
               </Link>
               <Link
-                href="/"
+                href="/foodbuddy"
                 activeClass="/"
-                to="/"
+                to="/foodbuddy"
                 smooth={true}
                 offset={50}
                 duration={500}
