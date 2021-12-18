e80583ebe5ecc598626028988bbe87911913914f
KevProDev <kevintheleader@yahoo.com>
2021-12-18 13:46:04 -0600

    nextauth keeping it default 8

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,7 +12,7 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       authorizationUrl:
-        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code",
+        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=online&response_type=code",
       scope:
         "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.readonly",
     }),
