a3e81a3d7c9c082d12bfdb60602a9fcf27b1c430
KevProDev <kevintheleader@yahoo.com>
2021-12-18 13:50:02 -0600

    nextauth keeping it default 9

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,10 +11,6 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorizationUrl:
-        "https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=online&response_type=code",
-      scope:
-        "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.readonly",
     }),
   ],
   adapter: PrismaAdapter(prisma),
@@ -27,32 +23,32 @@ export default NextAuth({
   // cookie: {
   //   secure: process.env.NODE_ENV && process.env.NODE_ENV === "production",
   // },
-  session: {
-    // Use JSON Web Tokens for session instead of database sessions.
-    jwt: true,
-    strategy: "database",
+  // session: {
+  //   // Use JSON Web Tokens for session instead of database sessions.
+  //   jwt: true,
+  //   strategy: "database",
 
-    // Seconds - How long until an idle session expires and is no longer valid.
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-  callbacks: {
-    async jwt({ token, user, account, profile, isNewUser }) {
-      // first time jwt callback is run, user object is available
-      if (account) {
-        token.accessToken = account.access_token;
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, user, token }) {
-      session.accessToken = token.accessToken;
-      session.id = token.id;
-      console.log(session);
-      return session;
-    },
-    // session: async (session, user) => {
-    //   session.id = user.id;
-    //   return Promise.resolve(session);
-    // },
-  },
+  //   // Seconds - How long until an idle session expires and is no longer valid.
+  //   maxAge: 30 * 24 * 60 * 60, // 30 days
+  // },
+  // callbacks: {
+  //   async jwt({ token, user, account, profile, isNewUser }) {
+  //     // first time jwt callback is run, user object is available
+  //     if (account) {
+  //       token.accessToken = account.access_token;
+  //       token.id = user.id;
+  //     }
+  //     return token;
+  //   },
+  //   async session({ session, user, token }) {
+  //     session.accessToken = token.accessToken;
+  //     session.id = token.id;
+  //     console.log(session);
+  //     return session;
+  //   },
+  //   // session: async (session, user) => {
+  //   //   session.id = user.id;
+  //   //   return Promise.resolve(session);
+  //   // },
+  // },
 });
