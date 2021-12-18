e45a1b4b9486142b19679c4d0d63577340ea7699
KevProDev <kevintheleader@yahoo.com>
2021-12-18 00:14:56 -0600

    change config/index

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://yelp-search-next-js-app.vercel.app";
+  : "https://www.meallocker.com/";
