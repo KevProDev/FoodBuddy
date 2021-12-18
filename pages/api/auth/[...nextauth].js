d28deff3f0b260788cccdd10d0f00ece055dac13
KevProDev <kevintheleader@yahoo.com>
2021-12-18 07:08:54 -0600

    change nextauth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -23,41 +23,40 @@ export default NextAuth({
   ],
   adapter: PrismaAdapter(prisma),
   secret: process.env.SECRET,
-  database: process.env.DATABASE_URL,
+  // database: process.env.DATABASE_URL,
   // cookie: {
   //   secure: process.env.NODE_ENV && process.env.NODE_ENV === "production",
   // },
-  jwt: {
-    encryption: true,
-    secret: process.env.SECRET,
-  },
-  session: {
-    // Use JSON Web Tokens for session instead of database sessions.
-    jwt: true,
-    strategy: "jwt",
-
-    // Seconds - How long until an idle session expires and is no longer valid.
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-  // pages: {
-  //   signIn: "/signin",
+  // jwt: {
+  //   encryption: true,
+  //   secret: process.env.SECRET,
   // },
-  callbacks: {
-    async jwt({ token, user, account, profile, isNewUser }) {
-      // first time jwt callback is run, user object is available
-      if (account) {
-        token.accessToken = account.access_token;
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, user, token }) {
-      session.accessToken = token.accessToken;
-      session.id = token.id;
-      console.log(session);
-      return session;
-    },
-  },
+  // session: {
+  //   // Use JSON Web Tokens for session instead of database sessions.
+  //   jwt: true,
+  //   strategy: "jwt",
+
+  //   // Seconds - How long until an idle session expires and is no longer valid.
+  //   maxAge: 30 * 24 * 60 * 60, // 30 days
+  // },
+  // callbacks: {
+  //   async jwt({ token, user, account, profile, isNewUser }) {
+  //     // first time jwt callback is run, user object is available
+  //     if (account) {
+  //       token.accessToken = account.access_token;
+  //       token.id = user.id;
+  //     }
+  //     return token;
+  //   },
+  //   async session({ session, user, token }) {
+  //     session.accessToken = token.accessToken;
+  //     session.id = token.id;
+  //     console.log(session);
+  //     return session;
+  //   },
+  // },
+
+  //////////
   // jwt: ({ token, user }) => {
   //   // first time jwt callback is run, user object is available
   //   if (user) {
