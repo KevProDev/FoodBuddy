c22ca7af39efd71d307b81fdabce7880f3ea64c0
KevProDev <kevintheleader@yahoo.com>
2021-12-18 07:48:17 -0600

    change nextauth.js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -27,32 +27,36 @@ export default NextAuth({
   // cookie: {
   //   secure: process.env.NODE_ENV && process.env.NODE_ENV === "production",
   // },
-  jwt: {
-    encryption: true,
-    secret: process.env.SECRET,
-  },
-  session: {
-    // Use JSON Web Tokens for session instead of database sessions.
-    jwt: true,
-    strategy: "jwt",
+  // jwt: {
+  //   encryption: true,
+  //   secret: process.env.SECRET,
+  // },
+  // session: {
+  //   // Use JSON Web Tokens for session instead of database sessions.
+  //   jwt: true,
+  //   strategy: "jwt",
 
-    // Seconds - How long until an idle session expires and is no longer valid.
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
+  //   // Seconds - How long until an idle session expires and is no longer valid.
+  //   maxAge: 30 * 24 * 60 * 60, // 30 days
+  // },
   callbacks: {
-    async jwt({ token, user, account, profile, isNewUser }) {
-      // first time jwt callback is run, user object is available
-      if (account) {
-        token.accessToken = account.access_token;
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session({ session, user, token }) {
-      session.accessToken = token.accessToken;
-      session.id = token.id;
-      console.log(session);
-      return session;
+    // async jwt({ token, user, account, profile, isNewUser }) {
+    //   // first time jwt callback is run, user object is available
+    //   if (account) {
+    //     token.accessToken = account.access_token;
+    //     token.id = user.id;
+    //   }
+    //   return token;
+    // },
+    // async session({ session, user, token }) {
+    //   session.accessToken = token.accessToken;
+    //   session.id = token.id;
+    //   console.log(session);
+    //   return session;
+    // },
+    session: async (session, user) => {
+      session.id = user.id;
+      return Promise.resolve(session);
     },
   },
 
