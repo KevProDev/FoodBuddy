acbe460c2647751ed5b338cf62c7d355d3a0b341
KevProDev <kevintheleader@yahoo.com>
2021-12-19 06:58:31 -0600

    stopping error on search

--- config/index.js
+++ config/index.js
@@ -2,4 +2,6 @@ const dev = process.env.NEXT_PUBLIC_VERCEL_ENV !== "production";
 
 console.log(dev);
 
-export const server = dev ? "http://localhost:3000" : "http://localhost:3000";
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://www.meallocker.com";
