84ff988c6428e299e373ac15d954ed8e0be469b0
KevProDev <kevintheleader@yahoo.com>
2021-12-25 13:10:21 -0600

    fixing image not showing up rest id page

--- config/index.js
+++ config/index.js
@@ -3,4 +3,4 @@ const dev = process.env.NEXT_PUBLIC_VERCEL_ENV !== "production";
 // export const server = dev ? "http://localhost:3000" : "http://localhost:3000";
 export const server = dev
   ? "http://localhost:3000"
-  : "http://www.meallocker.com";
+  : "https://www.meallocker.com";
