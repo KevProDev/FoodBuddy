3207603e035debcdb83c4e23c4ce8ac33cd7f2dc
KevProDev <kevintheleader@yahoo.com>
2022-01-18 16:23:35 -0600

    fix header stickyness as you scroll

--- pages/header.js
+++ pages/header.js
@@ -17,100 +17,101 @@ export default function Header() {
   };
 
   return (
-    <header className="sticky top-0 z-50 max-w-7xl mx-auto grid grid-cols-2 py-5 px-5 md:px-10 bg-white lg:grid-cols-5">
-      {/* Left */}
-      <div className="relative flex items-center h-10 cursor-pointer my-auto col-span-1">
-        <Link href="/">
-          <a href="">
-            <img src="/BLACK.png" alt="company logo" />
-          </a>
-        </Link>
-      </div>
+    <header className="bg-white sticky top-0 z-50">
+      <nav className="max-w-7xl mx-auto grid grid-cols-2 py-3 px-5 md:px-10 bg-white lg:grid-cols-5">
+        {/* Left */}
+        <div className="relative flex items-center h-10 cursor-pointer my-auto col-span-1">
+          <Link href="/">
+            <a href="">
+              <img src="/BLACK.png" alt="company logo" />
+            </a>
+          </Link>
+        </div>
 
-      {/* Middle */}
-      {/* <div className="hidden lg:flex items-center py-2 font-semibold col-span-2 justify-end pr-5 list-none space-x-8 text-lg ">
+        {/* Middle */}
+        {/* <div className="hidden lg:flex items-center py-2 font-semibold col-span-2 justify-end pr-5 list-none space-x-8 text-lg ">
         <Link href="/restaurantsearch">
           <a href="">Restaurant Search</a>
         </Link>
       </div> */}
-      {/* Right */}
-      <div className="flex grid-cols-2 items-center space-x-4 justify-end text-gray-600 col-span-1 lg:col-span-4">
-        {!session && (
-          <div className="flex items-center space-x-4">
-            {/* <Link href="/restaurantsearch">
+        {/* Right */}
+        <div className="flex grid-cols-2 items-center space-x-4 justify-end text-gray-600 col-span-1 lg:col-span-4">
+          {!session && (
+            <div className="flex items-center space-x-4">
+              {/* <Link href="/restaurantsearch">
               <a href="" className="font-bold hidden lg:inline-block">
                 Restaurant Search
               </a>
             </Link> */}
-            <p
-              className="hidden lg:inline-flex col-span-1 cursor-pointer text-md rounded-2xl py-2 px-2 "
-              onClick={loginWithGoogle}
-            >
-              Sign in
-              {session && `as ${session.user.name}`}
-            </p>
-            {/* <p
+              <p
+                className="hidden lg:inline-flex col-span-1 cursor-pointer text-md rounded-2xl py-2 px-2 "
+                onClick={loginWithGoogle}
+              >
+                Sign in
+                {session && `as ${session.user.name}`}
+              </p>
+              {/* <p
               className="hidden lg:inline-flex col-span-1 cursor-pointer text-md rounded-2xl py-2"
               onClick={logoutWithGoogle}
             >
               Sign Out
             </p> */}
-          </div>
-        )}
-        {session && (
-          <div className="space-x-4">
-            {/* <Link href="/restaurantsearch">
+            </div>
+          )}
+          {session && (
+            <div className="space-x-4">
+              {/* <Link href="/restaurantsearch">
               <a href="" className=" font-bold hidden lg:inline-block">
                 Restaurant Search
               </a>
             </Link> */}
-            {/* <p
+              {/* <p
               className="hidden lg:inline-flex col-span-1 cursor-pointer text-md rounded-2xl py-2"
               onClick={loginWithGoogle}
             >
               {session.user.name}
             </p> */}
-            <p
-              className="hidden lg:inline-flex col-span-1 cursor-pointer text-md rounded-2xl py-2 px-2"
-              onClick={logoutWithGoogle}
-            >
-              Sign Out
-            </p>
-            <Link href="/profile">
-              <a
-                href=""
-                className=" font-bold hidden lg:inline-block py-2 px-2"
+              <p
+                className="hidden lg:inline-flex col-span-1 cursor-pointer text-md rounded-2xl py-2 px-2"
+                onClick={logoutWithGoogle}
               >
-                Profile
-              </a>
-            </Link>
-          </div>
-        )}
-        <button
-          className="flex lg:hidden items-center space-x-2 border-2 rounded-full p-2"
-          onClick={() => setIsOpen(!isOpen)}
-          type="button"
-          aria-controls="mobile-menu"
-          aria-expanded="true"
+                Sign Out
+              </p>
+              <Link href="/profile">
+                <a
+                  href=""
+                  className=" font-bold hidden lg:inline-block py-2 px-2"
+                >
+                  Profile
+                </a>
+              </Link>
+            </div>
+          )}
+          <button
+            className="flex lg:hidden items-center space-x-2 border-2 rounded-full p-2"
+            onClick={() => setIsOpen(!isOpen)}
+            type="button"
+            aria-controls="mobile-menu"
+            aria-expanded="true"
+          >
+            <MenuIcon className="h-6" />
+            <UserCircleIcon className="h-8" />
+          </button>
+        </div>
+        <Transition
+          className="flex col-span-3"
+          show={isOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
         >
-          <MenuIcon className="h-6" />
-          <UserCircleIcon className="h-8" />
-        </button>
-      </div>
-      <Transition
-        className="flex col-span-3"
-        show={isOpen}
-        enter="transition ease-out duration-100 transform"
-        enterFrom="opacity-0 scale-95"
-        enterTo="opacity-100 scale-100"
-        leave="transition ease-in duration-75 transform"
-        leaveFrom="opacity-100 scale-100"
-        leaveTo="opacity-0 scale-95"
-      >
-        {() => (
-          <div className="lg:hidden id:mobile-menu">
-            <div className="flex flex-col px-2 pt-2 pb-3 space-y-1">
-              {/* <Link href="/">
+          {() => (
+            <div className="lg:hidden id:mobile-menu">
+              <div className="flex flex-col px-2 pt-2 pb-3 space-y-1">
+                {/* <Link href="/">
                 <a
                   onClick={() => setIsOpen(!isOpen)}
                   href=""
@@ -119,7 +120,7 @@ export default function Header() {
                   Home
                 </a>
               </Link> */}
-              {/* <Link href="/restaurantsearch">
+                {/* <Link href="/restaurantsearch">
                 <a
                   onClick={() => setIsOpen(!isOpen)}
                   href=""
@@ -129,48 +130,49 @@ export default function Header() {
                 </a>
               </Link> */}
 
-              {!session && (
-                <div className="flex flex-col items-start">
-                  <p
-                    className=" font-bold cursor-pointer text-xl rounded-2xl py-2"
-                    onClick={loginWithGoogle}
-                  >
-                    Sign In
-                    {session && `as ${session.user.name}`}
-                  </p>
-                  {/* <p
+                {!session && (
+                  <div className="flex flex-col items-start">
+                    <p
+                      className=" font-bold cursor-pointer text-xl rounded-2xl py-2"
+                      onClick={loginWithGoogle}
+                    >
+                      Sign In
+                      {session && `as ${session.user.name}`}
+                    </p>
+                    {/* <p
                     className=" text-xl font-bold  cursor-pointer  rounded-2xl py-2"
                     onClick={logoutWithGoogle}
                   >
                     Sign Out
                   </p> */}
-                </div>
-              )}
-              {session && (
-                <div className="flex flex-col items-start">
-                  <p
-                    className="font-bold cursor-pointer text-xl rounded-2xl py-2"
-                    onClick={loginWithGoogle}
-                  >
-                    {session.user.name}
-                  </p>
-                  <p
-                    className="font-bold cursor-pointer text-xl rounded-2xl py-2"
-                    onClick={logoutWithGoogle}
-                  >
-                    Sign Out
-                  </p>
-                  <Link href="/profile">
-                    <a href="" className=" font-bold hidden lg:inline-block">
-                      Profile
-                    </a>
-                  </Link>
-                </div>
-              )}
+                  </div>
+                )}
+                {session && (
+                  <div className="flex flex-col items-start">
+                    <p
+                      className="font-bold cursor-pointer text-xl rounded-2xl py-2"
+                      onClick={loginWithGoogle}
+                    >
+                      {session.user.name}
+                    </p>
+                    <p
+                      className="font-bold cursor-pointer text-xl rounded-2xl py-2"
+                      onClick={logoutWithGoogle}
+                    >
+                      Sign Out
+                    </p>
+                    <Link href="/profile">
+                      <a href="" className=" font-bold hidden lg:inline-block">
+                        Profile
+                      </a>
+                    </Link>
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-        )}
-      </Transition>
+          )}
+        </Transition>
+      </nav>
     </header>
   );
 }
